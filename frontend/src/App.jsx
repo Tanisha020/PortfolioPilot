131f9cad76a1ad74dc8e1e9e0516dd5aa5589f1f
Tanisha020 <tanisha2318@gmail.com>
2025-03-09 23:32:24 +0530

    initial setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/Login/LoginPage.jsx";
+import PageNotFound from "./pages/PageNotFound/PageNotFound.jsx";
+import Layout from "./Layout/Homelayout.jsx";
+import RegisterPage from "./pages/Register/RegisterPage.jsx";
+import Home from "./pages/Home/Home.jsx";
+// import Dashboard from "./pages/Dashboard/Dashboard.jsx";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+        </Route>
+
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
